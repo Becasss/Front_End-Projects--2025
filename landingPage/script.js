27d1e2a0a12f78effb81649d4c80d640940d1eed
w3schools-test <raeebikash@gmail.com>
2025-02-19 16:12:36 +0545

    Updating script .js

--- landingPage/script.js
+++ landingPage/script.js
@@ -6,4 +6,23 @@ mobileMenu.addEventListener('click', () => {
     navlinks.classList.toggle('active');
 });
 
+// Smooth scrolling for anchor links
+document.querySelector('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Form submission handling
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank You for your message! we will get back to you soon.');
+    contactForm.reset();
+});
+
 
